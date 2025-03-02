c56113428d06c8afb6388cc5cef5dc2abd08f49b
viktormih <chronosroseviknr@gmail.com>
2025-03-02 16:06:01 +0300

    First solution.

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -2,19 +2,20 @@ package lesson_2
 
 fun main() {
 
-    val employees = 50
+    val workers = 50
     val interns = 30
 
-    val employeesSalary = 30_000
+    val workersSalary = 30_000
     val internsSalary = 20_000
 
-    val employeesExpenses = employeesSalary * employees
-    val everySalaryExpenses = (internsSalary * interns) + employeesExpenses
+    val workersSalaryExpenses = workersSalary * workers
+    val internsSalaryExpenses = internsSalary * interns
+    val everySalaryExpenses = internsSalaryExpenses + workersSalaryExpenses
 
-    val averageSalary = everySalaryExpenses / (interns + employees)
+    val averageSalary = everySalaryExpenses / (interns + workers)
 
     val answer = """
-        Расходы на выплату зарплаты постоянных сотрудников: $employeesExpenses
+        Расходы на выплату зарплаты постоянных сотрудников: $workersSalaryExpenses
         Общие расходы по ЗП после прихода стажеров: $everySalaryExpenses
         Средняя ЗП одного сотрудника после устройства стажеров: $averageSalary
     """.trimIndent()
