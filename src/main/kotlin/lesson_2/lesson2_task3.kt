a4c9a65bf38936e2375bbded13481d2436c38b8d
viktormih <chronosroseviknr@gmail.com>
2025-03-02 16:11:14 +0300

    First solution.

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,16 +1,19 @@
 package lesson_2
 
+const val minutesInHour = 60
+
 fun main() {
 
-    val serverReport = "поезд выехал в 9:39"
+    val serverReport = "Поезд выехал в 9:39"
     val departureHours = 9
     val departureMinutes = 39
 
     val travelTimeInMinutes = 457
     val totalMinutes = travelTimeInMinutes + departureMinutes
 
-    val arrivalHours = departureHours + (totalMinutes / 60)
-    val arrivalMinutes = totalMinutes % 60
+    val arrivalHours = departureHours + (totalMinutes / minutesInHour)
+    val arrivalMinutes = totalMinutes % minutesInHour
 
-    println("Поезд прибудет к $arrivalHours:$arrivalMinutes")
+    println(serverReport)
+    print("Поезд прибудет к $arrivalHours:$arrivalMinutes")
 }
