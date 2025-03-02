55ebf6188dd708236f097433e31a804e48d289a5
viktormih <chronosroseviknr@gmail.com>
2025-03-02 15:34:25 +0300

    Fixed magic numbers and useless comments

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,10 +2,12 @@ package lesson_1
 
 fun main() {
     val secondsInSpace = 6480
+    val secondsInHour = 3600
+    val minutesInHour = 60
 
-    val hours = secondsInSpace / 3600                   //1
-    val minutes = (secondsInSpace % 3600) / 60          //  2880 / 60
-    val seconds = secondsInSpace % 60                   //0
+    val hours = secondsInSpace / secondsInHour
+    val minutes = (secondsInSpace % secondsInHour) / minutesInHour
+    val seconds = secondsInSpace % minutesInHour
 
     val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
     println(formattedTime)
