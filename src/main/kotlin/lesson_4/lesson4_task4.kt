8bb51c5a2ca04668a25aaa7100d7ef2d51a4823d
viktormih <chronosroseviknr@gmail.com>
2025-03-06 15:50:33 +0300

    Second solution. Флаг для оптимизации + авто форматирование.

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -3,16 +3,22 @@ package lesson_4
 fun main() {
 
     var dayCount = 5
+    var isEven = dayCount % 2 == 0
 
-    println("Упражнения для рук: ${(dayCount % 2 !== 0)} \n" +
-            "Упражнения для ног: ${(dayCount % 2 == 0)} \n" +
-            "Упражнения для спины: ${(dayCount % 2 == 0)} \n" +
-            "Упражнения для пресса: ${(dayCount % 2 !== 0)} \n")
+    println(
+        "Упражнения для рук: ${!isEven} \n" +
+                "Упражнения для ног: $isEven \n" +
+                "Упражнения для спины: $isEven \n" +
+                "Упражнения для пресса: ${!isEven} \n"
+    )
 
     dayCount++
+    isEven = dayCount % 2 == 0
 
-    println("Упражнения для рук: ${(dayCount % 2 !== 0)} \n" +
-            "Упражнения для ног: ${(dayCount % 2 == 0)} \n" +
-            "Упражнения для спины: ${(dayCount % 2 == 0)} \n" +
-            "Упражнения для пресса: ${(dayCount % 2 !== 0)} \n")
+    println(
+        "Упражнения для рук: ${!isEven} \n" +
+                "Упражнения для ног: $isEven \n" +
+                "Упражнения для спины: $isEven \n" +
+                "Упражнения для пресса: ${!isEven} \n"
+    )
 }
