04a949bef4cc9bcc7dd41d0801cdf72d81256f67
viktormih <chronosroseviknr@gmail.com>
2025-03-06 16:01:05 +0300

    First solution

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,19 +1,16 @@
 package lesson_4
 
-import java.util.Scanner
+import java.util.*
 
 const val CONDITION_IS_DAMAGED = false
 const val CONDITION_PROVISION_BOXES = 50
-const val CONDITION_FAVORABLE_WEATHER = true
-const val CONDITION_UNFAVORABLE_WEATHER = true
+const val CONDITION_IS_FAVORABLE = true
+const val CONDITION_IS_UNFAVORABLE = true
 
 const val CONDITION_DAMAGED_SHIP_CREW_SIZE = 70
 
 fun main() {
-//- наличие повреждений корпуса (Boolean переменная);
-//- текущий состав экипажа;
-//- количество ящиков с провизией на борту;
-//- благоприятность метеоусловий (Boolean переменная).
+
     val scanner = Scanner(System.`in`)
 
     println("Корпус корабля поврежден? [true/false]")
@@ -27,22 +24,21 @@ fun main() {
     val provisionBoxesNum = scanner.nextInt()
 
     println("Погода благоприятная? [true/false]")
-    val favorableWeather = scanner.nextBoolean()
+    val isFavorable = scanner.nextBoolean()
 
     if ((isDamaged == CONDITION_IS_DAMAGED) &&
         (conditionCrewSize) &&
         (provisionBoxesNum > CONDITION_PROVISION_BOXES) &&
-        (favorableWeather == CONDITION_FAVORABLE_WEATHER || favorableWeather == CONDITION_UNFAVORABLE_WEATHER)
+        (isFavorable == CONDITION_IS_FAVORABLE || isFavorable == CONDITION_IS_UNFAVORABLE)
     ) {
-        println("Корабль может отплыть! Удачного плавания!")
+        println("Success!! Корабль может отплыть! Удачного плавания!")
     } else if ((!isDamaged == CONDITION_IS_DAMAGED) &&
         (crewSize == CONDITION_DAMAGED_SHIP_CREW_SIZE) &&
-        (favorableWeather == CONDITION_FAVORABLE_WEATHER ||
-                favorableWeather == CONDITION_UNFAVORABLE_WEATHER) &&
-        (provisionBoxesNum > CONDITION_PROVISION_BOXES)
+        (provisionBoxesNum > CONDITION_PROVISION_BOXES) &&
+        (isFavorable == CONDITION_IS_FAVORABLE || isFavorable == CONDITION_IS_UNFAVORABLE)
     ) {
-        println("Корабль может отплыть! Удачного плавания!")
+        println("Success!! Корабль может отплыть! Удачного плавания!")
     } else {
-        println("Корабль не может отплыть! Подготовьтесь лучше!")
+        println("Fail!! Корабль не может отплыть! Подготовьтесь лучше!")
     }
 }
